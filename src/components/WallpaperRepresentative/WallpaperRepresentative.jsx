2e8ebf617196a6cf5ce6578499a2232307b9228d
Trịnh Đồng <trinhdong255@gmail.com>
2025-03-06 23:06:41 +0700

    Create component Wallpaper Representative let extends other children component

---
+++ src/components/WallpaperRepresentative/WallpaperRepresentative.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types"
+import { Stack } from "@mui/material";
+
+const WallpaperRepresentative = ({titleHeader}) => {
+  return (
+    <Stack position={"relative"}>
+      <img
+        style={{ width: "100%", height: "100%" }}
+        src="/src/assets/images/backgroundFashions/background-fashion.jpg"
+        alt="Background Fashion"
+      />
+      <Stack
+        position={"absolute"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        sx={{
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "3rem",
+            color: "white",
+            margin: 0,
+          }}
+        >
+        {titleHeader}
+        </h1>
+      </Stack>
+    </Stack>
+  );
+};
+
+// Checking datatype is correct or not
+WallpaperRepresentative.propTypes = {
+    titleHeader: PropTypes.string.isRequired,
+  };
+
+export default WallpaperRepresentative;
