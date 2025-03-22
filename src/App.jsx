1ffa568109582e3050b2e835ae76965acfe0eb3d
Trịnh Đồng <trinhdong255@gmail.com>
2025-03-22 16:39:15 +0700

    Add route ListProducts, DetailProducts and component of ScrollToTop whole page when scroll down at top header

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,50 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/HomePage/HomePage";
-import Contact from "./pages/ContactPage/ContactPage";
-import Support from "./pages/SupportPage/SupportPage";
-import About from "./pages/AboutPage/AboutPage";
-import Login from "./pages/LoginPage/LoginPage";
-import SignUp from "./pages/SignUpPage/SignUpPage";
+import Home from "./pages/HomePage/Home";
+import Contact from "./pages/ContactPage/Contact";
+import Support from "./pages/SupportPage/Support";
+import About from "./pages/AboutPage/About";
+import Login from "./pages/LoginPage/Login";
+import SignUp from "./pages/SignUpPage/SignUp";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import MainLayout from "./layouts/MainLayout/MainLayout";
 import LoginLayout from "./layouts/LoginLayout/LoginLayout";
 import ListProductsLayout from "./layouts/ListProductsLayout/ListProductsLayout";
-import ListProducts from "./pages/ListProductsPage/ListProductsPage";
-import VerifyAccount from "./pages/VerifyAccount/VerifyAccount";
+import ListProducts from "./pages/ListProductsPage/ListProducts";
+import VerifyAccount from "./pages/VerifyAccountPage/VerifyAccount";
+import DetailProducts from "./pages/DetailProductsPage/DetailProducts";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <Routes>
-      {/* Route dependencies component Header and Footer */}
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="support" element={<Support />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="about" element={<About />} />
-      </Route>
+    <>
+      <ScrollToTop />
+      <Routes>
+        {/* Route dependencies component Header and Footer */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="support" element={<Support />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+        </Route>
 
-      {/* Route Login */}
-      <Route path="/login" element={<LoginLayout />}>
-        <Route index element={<Login />} />
-        <Route path="forgotPassword" element={<ForgotPassword />} />
-      </Route>
+        {/* Route Login */}
+        <Route path="/login" element={<LoginLayout />}>
+          <Route index element={<Login />} />
+          <Route path="forgotPassword" element={<ForgotPassword />} />
+        </Route>
 
-      {/* Route SignUp and VerifyAccount */}
-      <Route path="/signUp" element={<SignUp />} />
-      <Route path="/verifyAccount" element={<VerifyAccount />} />
+        {/* Route SignUp and VerifyAccount */}
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/verifyAccount" element={<VerifyAccount />} />
 
-      {/* Route List Products */}
-      <Route path="/listProducts" element={<ListProductsLayout />}>
-        <Route index element={<ListProducts />} />
-      </Route>
-    </Routes>
+        {/* Route List Products */}
+        <Route path="/listProducts" element={<ListProductsLayout />}>
+          <Route index element={<ListProducts />} />
+        </Route>
+
+        <Route path="/detailProducts/:id" element={<DetailProducts />} />
+      </Routes>
+    </>
   );
 }
 
