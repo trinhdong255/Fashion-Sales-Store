a4f5fddbd39ceb41911c67eaef746c62d60cdd17
JamesDong <james@vinova.com.sg>
2025-03-30 11:58:27 +0700

    setup: install redux and restructure project

--- src/App.jsx
+++ src/App.jsx
@@ -11,45 +11,40 @@ import LoginLayout from "./layouts/LoginLayout/LoginLayout";
 import VerifyAccount from "./pages/VerifyAccountPage/VerifyAccount";
 import ProductDetails from "./pages/ProductDetailsPage/ProductDetails";
 import ScrollToTop from "./components/ScrollToTop";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ProductListsLayout from "./layouts/ProductListsLayout/ProductListsLayout";
-import ProductLists from "./pages/ProductListsPage/ProductLists";
-
-const queryClient = new QueryClient();
+import ProductListsLayout from "@/layouts/ProductListsLayout";
+import ProductLists from "@/pages/ProductListsPage/ProductLists";
 
 const App = () => {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <ScrollToTop />
-        <Routes>
-          {/* Route dependencies component Header and Footer */}
-          <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="support" element={<Support />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="about" element={<About />} />
-          </Route>
+      <ScrollToTop />
+      <Routes>
+        {/* Route dependencies component Header and Footer */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="support" element={<Support />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+        </Route>
 
-          {/* Route Login */}
-          <Route path="/login" element={<LoginLayout />}>
-            <Route index element={<Login />} />
-            <Route path="forgotPassword" element={<ForgotPassword />} />
-          </Route>
+        {/* Route Login */}
+        <Route path="/login" element={<LoginLayout />}>
+          <Route index element={<Login />} />
+          <Route path="forgotPassword" element={<ForgotPassword />} />
+        </Route>
 
-          {/* Route SignUp and VerifyAccount */}
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/verifyAccount" element={<VerifyAccount />} />
+        {/* Route SignUp and VerifyAccount */}
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/verifyAccount" element={<VerifyAccount />} />
 
-          {/* Route List Products */}
+        {/* Route List Products */}
 
-          <Route path="/listProducts" element={<ProductListsLayout />}>
-            <Route index element={<ProductLists />} />
-          </Route>
+        <Route path="/listProducts" element={<ProductListsLayout />}>
+          <Route index element={<ProductLists />} />
+        </Route>
 
-          <Route path="/productDetails/:id" element={<ProductDetails />} />
-        </Routes>
-      </QueryClientProvider>
+        <Route path="/productDetails/:id" element={<ProductDetails />} />
+      </Routes>
     </>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +10,12 @@ export default defineConfig({
     modules: {
       scopeBehaviour: "local", // CSS mặc định là scoped/local
       generateScopedName: "[name]__[local]___[hash:base64:5]", // Tên class CSS module
-      localsConvention: "camelCase", // Tên class CSS module 
+      localsConvention: "camelCase", // Tên class CSS module
     },
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
+    },
+  },
+});
