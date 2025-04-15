da486197a3fa0094627faf9f810cb6aa23eacfa3
Trịnh Đồng <trinhdong255@gmail.com>
2025-04-15 20:25:26 +0700

    feat(api): change controller

--- src/services/api/product.js
+++ src/services/api/product.js
@@ -1,31 +1,55 @@
+// productApi.js
 import { baseApi } from "/src/services/api/index.js";
-// import { TAG_KEYS } from "/src/constants/tagKeys.js?t=1744269010633&t=1744269010633";
 import { TAG_KEYS } from "/src/constants/tagKeys.js";
 
 export const productApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     listProducts: builder.query({
       query: (params) => {
-        // Kiểm tra params, nếu không tồn tại thì dùng giá trị mặc định
         const { skip = 0, limit = 20 } = params || {};
         return {
-          url: `/products?limit=${limit}&skip=${skip}`,
+          url: `/v1/products?limit=${limit}&skip=${skip}`,
         };
       },
       providesTags: [TAG_KEYS.PRODUCT],
+      // Refetch khi tham số query thay đổi
+      keepUnusedDataFor: 60, // Giữ dữ liệu trong cache 60 giây
+      refetchOnMountOrArgChange: true, // Refetch khi tham số thay đổi
     }),
+
+    searchProducts: builder.query({
+      query: (params) => {
+        const { q, skip = 0, limit = 20 } = params || {};
+        if (!q) {
+          throw new Error("Search term is required");
+        }
+        return {
+          url: `/v1/products/search?q=${q}&limit=${limit}&skip=${skip}`,
+        };
+      },
+      providesTags: [TAG_KEYS.PRODUCT],
+      keepUnusedDataFor: 60,
+      refetchOnMountOrArgChange: true,
+    }),
+    
     getProductById: builder.query({
       query: (id) => {
         if (!id) {
           throw new Error("Product ID is required");
         }
         return {
-          url: `/products/${id}`,
+          url: `/v1/products/${id}`,
         };
       },
       providesTags: [TAG_KEYS.PRODUCT],
+      keepUnusedDataFor: 60,
+      refetchOnMountOrArgChange: true,
     }),
   }),
 });
 
-export const { useListProductsQuery, useGetProductByIdQuery } = productApi;
+export const {
+  useListProductsQuery,
+  useSearchProductsQuery,
+  useGetProductByIdQuery,
+} = productApi;
