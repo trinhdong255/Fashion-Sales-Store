def7232f0391a7168a705f3ee8694596fee6cc7d
Trịnh Đồng <trinhdong255@gmail.com>
2025-06-29 09:56:00 +0700

    feat: handle fetch all branches for user

---
+++ src/services/api/branches.js
@@ -0,0 +1,15 @@
+import { TAG_KEYS } from "@/constants/tagKeys";
+import { baseApi } from "./index";
+
+export const branchesApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getListBranches: builder.query({
+      query: () => ({
+        url: "/v1/public/branches",
+      }),
+      providesTags: [TAG_KEYS.BRANCHES],
+    }),
+  }),
+});
+
+export const { useGetListBranchesQuery } = branchesApi;
